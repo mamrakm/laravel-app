e081869f00ec42469e8f1932213812305ea2a30b
EMeMSoft s.r.o. <miroslav.mamrak@seznam.cz>
2025-01-10 07:35:27 +0100

    feat: added authentication to the application

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,25 @@
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
+// Protect all routes with 'auth' middleware
+Route::middleware(['auth'])->group(function () {
+    // Dashboard route
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // Customer routes
+    Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
+    Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
+    Route::post('/customers', [CustomerController::class, 'store'])->name('customers.store');
+    Route::get('/customers/{customer}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
+    Route::put('/customers/{customer}', [CustomerController::class, 'update'])->name('customers.update');
+    Route::delete('/customers/{customer}', [CustomerController::class, 'destroy'])->name('customers.destroy');
+});
+
+// Public route (if any)
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
-Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
-Route::post('/customers', [CustomerController::class, 'store'])->name('customers.store');
+require __DIR__.'/auth.php';
