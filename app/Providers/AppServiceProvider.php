940275665003823b9ab1786fb0b0c901b737d685
EMeMSoft s.r.o. <miroslav.mamrak@seznam.cz>
2025-01-18 23:35:24 +0100

    fix: added bind for CustomerSevice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,17 @@
 namespace App\Providers;
 
 use App\Models\Customer;
+use App\Repositories\CustomerRepository;
 use App\Repositories\CustomerRepositoryImpl;
-use App\Services\CustomerServiceImpl;
 use App\Services\CustomerService;
+use App\Services\CustomerServiceImpl;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(CustomerRepositoryImpl::class, fn () => new CustomerRepositoryImpl(new Customer()));
+        $this->app->singleton(CustomerRepository::class, fn () => new CustomerRepositoryImpl(new Customer()));
         $this->app->bind(CustomerService::class, CustomerServiceImpl::class);    }
 
     public function boot(): void
